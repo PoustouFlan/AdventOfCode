c4120b8c9fe4e1ca44e49e68afb95fd674702a07
Quentin Rataud <quentin.rataud@epita.fr>
2023-12-01 07:04:44 +0100

    2023: add day1 solution

---
+++ 2023/1/solve.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.insert(0, "/home/poustouflan/CP/AdventOfCode/lib")
+from aoc import submit
+
+done = False
+digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+res = 0
+while not done:
+    try:
+        line = input()
+        n = ''
+
+        ## Lecture du premier chiffre
+        while len(n) == 0:
+            for x in range(10):
+                if line[0] == str(x) or line.startswith(digits[x]):
+                    n += str(x)
+                    break
+            else:
+                line = line[1:]
+
+        ## Lecture du dernier chiffre
+        while len(n) == 1:
+            for x in range(10):
+                if line[-1] == str(x) or line.endswith(digits[x]):
+                    n += str(x)
+                    break
+            else:
+                line = line[:-1]
+        res += int(n)
+
+    except EOFError:
+        ## Fin de l'entrée
+        done = True
+
+submit(res)
