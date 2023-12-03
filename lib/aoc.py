d94ddda42eb1b2da98862f054e8bc581956fb8ca
Quentin Rataud <quentin.rataud@epita.fr>
2023-12-03 06:59:38 +0100

    aoc: fix auto-submit on both levels

--- lib/aoc.py
+++ lib/aoc.py
@@ -15,23 +15,31 @@ with open(cookie_path, "r") as cookie_file:
     cookie = cookie_file.read().strip()
 
 
-def submit(value, level=1, year=None, day=None):
+def submit(value, level=None, day=None, year=None):
     """
     Automatically submit a value for a challenge.
     year/day are set to current day if not provided.
     level = 1 for the gray star, level = 2 for the golden star.
+    level = None for attempting both.
     """
     if year is None:
         year = date.today().year
+
+    if day is None:
         day = date.today().day
 
-    submit_url = f"https://adventofcode.com/{year}/day/{day}/answer"
-    debug("Submitting to", submit_url, "...")
-    payload = {
-        "level": level,
-        "answer": value,
-    }
-    r = requests.post(submit_url, payload, cookies={"cookie":cookie})
-    debug(r.status_code)
-    debug(r.text)
-    return r.status_code == 200
+    if level is None:
+        levels = [2, 1]
+    else:
+        levels = [level]
+
+    for level in levels:
+        submit_url = f"https://adventofcode.com/{year}/day/{day}/answer"
+        debug("Submitting", value, "to", submit_url, "...")
+        payload = {
+            "level": level,
+            "answer": value,
+        }
+        r = requests.post(submit_url, payload, cookies={"cookie":cookie})
+        debug(r.status_code)
+        debug(r.text)
