99cf81834a584e9481c389b53104ba16cb245f9f
Quentin Rataud <quentin.rataud@epita.fr>
2023-12-06 14:15:50 +0100

    2023: fix day6 star2

--- 2023/6/star2.py
+++ 2023/6/star2.py
@@ -4,6 +4,14 @@ from aoc import submit
 
 from math import isqrt
 
+def ceil_sqrt(x):
+    # Returns ceil(sqrt(x)) with only integer arithmetics.
+    candidate = isqrt(x)
+    if candidate * candidate == x:
+        return candidate
+    else:
+        return candidate + 1
+
 # Input
 time = int(input().replace(' ', '').split(':')[1])
 distance = int(input().replace(' ', '').split(':')[1])
@@ -11,15 +19,18 @@ distance = int(input().replace(' ', '').split(':')[1])
 # Main computations
 
 #   (time - x) * x > distance
-# ⇒ -x² + time*x - distance > 0
+# ⇒ x² - time*x + distance > 0
 # ⇒ Quadratic equation
-a = -1
-b = time
-c = -distance
+a = 1
+b = -time
+c = distance
 
 delta = b*b - 4*a*c
-x1 = (-b - isqrt(delta)) // (2*a)
-x2 = (-b + isqrt(delta)) // (2*a)
-# All hold time between x2 and x1 will beat the record
+x1 = (-b - ceil_sqrt(delta)) // (2*a)             # Rounded down
+x2 = (-b + ceil_sqrt(delta) + (2*a - 1)) // (2*a) # Rounded up
 
-submit(x1 - x2, 2, 6, 2023)
+# All hold time between x1 and x2 will beat the record
+# (both x1 and x2 are excluded)
+# ⇒ x2 - x1 - 1 solutions
+
+submit(x2 - x1 - 1, 2, 6, 2023)
