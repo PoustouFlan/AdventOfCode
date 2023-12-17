5508632cd83db81617bc5c9aa93bc7bb65116b79
Quentin Rataud <quentin.rataud@epita.fr>
2023-12-18 00:09:08 +0100

    2023: use Dijkstra for day17

--- 2023/17/star2.py
+++ 2023/17/star2.py
@@ -2,6 +2,8 @@ import sys
 sys.path.insert(0, "../../lib")
 from aoc import submit
 
+from heapq import *
+
 ## Constants
 NORTH = (0, -1)
 SOUTH = (0, 1)
@@ -36,45 +38,44 @@ min_heat_loss = [
 # not being able to continue toward DIRS[dir_index]
 # (probably because it was the last move)
 
-# min_heat_loss initialization
-level = []
+# Dijkstra's algorithm
+# Heap stores (heat loss, x, y, last direction)
+heap = []
 for dir_index in range(4):
-    min_heat_loss[0][0][dir_index] = 0
-    level.append((0, 0, dir_index))
+    heappush(heap, (0, 0, 0, dir_index))
 
-# level-order breadth-first search
-while len(level) > 0:
-    next_level = []
-    
-    # Level processing
-    for x, y, forbidden_dir in level:
+# Dijkstra's algorithm
+while len(heap) > 0:
+    heat_loss, x, y, forbidden_dir = heappop(heap)
+    if min_heat_loss[y][x][forbidden_dir] <= heat_loss:
+        # outdated value
+        continue
+    min_heat_loss[y][x][forbidden_dir] = heat_loss
 
-        # Vertex processing
-        for dir_index in range(4):
-            heat_loss = min_heat_loss[y][x][forbidden_dir]
+    # Vertex processing
+    for dir_index in range(4):
+        # Recover heat_loss value
+        heat_loss = min_heat_loss[y][x][forbidden_dir]
 
-            # We cannot continue toward the same direction,
-            # nor in the opposite direction.
-            if dir_index == forbidden_dir or dir_index == forbidden_dir ^ 1:
+        # We cannot continue toward the same direction,
+        # nor in the opposite direction.
+        if dir_index == forbidden_dir or dir_index == forbidden_dir ^ 1:
+            continue
+
+        dx, dy = DIRECTIONS[dir_index]
+        for move in range(1, 11):
+            rx = x + move * dx
+            ry = y + move * dy
+            if not (0 <= rx < width and 0 <= ry < height):
+                # Out of bounds
+                break
+
+            heat_loss += grid[ry][rx]
+
+            # Minimal displacement of 4
+            if move <= 3:
                 continue
 
-            dx, dy = DIRECTIONS[dir_index]
-            for move in range(1, 11):
-                rx = x + move * dx
-                ry = y + move * dy
-                if not (0 <= rx < width and 0 <= ry < height):
-                    # Out of bounds
-                    break
-
-                heat_loss += grid[ry][rx]
-                # Minimal displacement of 4
-                if move <= 3:
-                    continue
-
-                if min_heat_loss[ry][rx][dir_index] > heat_loss:
-                    min_heat_loss[ry][rx][dir_index] = heat_loss
-                    next_level.append((rx, ry, dir_index))
-
-    level = next_level
+            heappush(heap, (heat_loss, rx, ry, dir_index))
 
 submit(min(min_heat_loss[-1][-1]), 2, 17, 2023)
